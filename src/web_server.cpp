e6fde4df26cf5606e792efacebb71a08201fe2b9
tritrann <tranminhtri25x1@gmail.com>
2026-07-30 12:00:01 +0700

    Update web_server: add clientsocket class and acceptConnection method

--- src/web_server.cpp
+++ src/web_server.cpp
@@ -28,3 +28,25 @@ ServerSocket::ServerSocket(std::string protocol, std::string ip, int p)
         exit(EXIT_FAILURE);
     }
 }
+
+ServerSocket::~ServerSocket() {
+    close(server_fd);
+}
+
+ServerSocket::acceptConnection(std::string &client_ip, int &client_port) {
+    sockaddr_in client_address;
+    socklen_t client_len = sizeof(client_address);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_len);
+
+    if (client_fd < 0) {
+        std::cerr << "Failed to accept connection" << std::endl;
+        return -1;
+    }
+
+    char ip_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_address.sin_addr, ip_str, sizeof(ip_str));
+    client_ip = std::string(ip_str);
+    client_port = ntohs(client_address.sin_port);
+
+    return client_fd;
+}
--- src/web_server.hpp
+++ src/web_server.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
 class ServerSocket {
@@ -18,4 +19,17 @@ public:
     ServerSocket(std::string protocol, std::string ip, int p);
     ~ServerSocket();
     int acceptConnection(std::string &client_ip, int &client_port);
+};
+
+class ClientSocket {
+private:
+    int client_fd;
+    std::string client_ip;
+    int client_port;
+public:
+    ClientSocket(int fd, const std::string &ip, int port);
+    ~ClientSocket();
+    ssize_t sendData(const std::string &data);
+    ssize_t receiveData(std::string &data);
+    ssize_t parse_http_request(std::string &request, std::string &method, std::string &path, std::string &version);
 };

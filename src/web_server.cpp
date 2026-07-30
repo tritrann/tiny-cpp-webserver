095ad9e4f4dbf6d93abf0be1e801960652457ccd
tritrann <tranminhtri25x1@gmail.com>
2026-07-30 11:34:00 +0700

    Update web_server file: Add construction

--- src/web_server.cpp
+++ src/web_server.cpp
@@ -5,17 +5,16 @@
 
 ServerSocket::ServerSocket(std::string protocol, std::string ip, int p)
     : protocol(protocol), source_ip(ip), source_port(p) {
-    // Create a socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
     if (server_fd == -1) {
         std::cerr << "Failed to create socket" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    // Bind the socket to the specified IP and port
     sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(source_ip.c_str());
+    inet_pton(AF_INET, source_ip.c_str(), &address.sin_addr);
     address.sin_port = htons(source_port);
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
